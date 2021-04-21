1a8bda65b141e110c8ccb76e361685c585eaa0a9
Patrick Gemmll <patrick.gemmell@mths.ca>
2021-04-21 14:36:24 +0000

    code v1

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+package ca.mths.unit1.unit101.java.hello_world;
+/**
+* The Hello World! program implements an application that
+* simply displays "Hello World!" to the standard output.
+*
+* @author  Patrick Gemmell
+* @version 1.0
+* @since   2021-04-20
+*/
+final class HelloWorld {
+    private HelloWorld() {
+        // Prevent instantiation
+        // Optional: throw an exception e.g. AssertionError
+        // if this ever *is* called
+    }
+    /**
+    * This method prints out "Hello, World!".
+    * @param args
+    */
+    public static void main(final String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
